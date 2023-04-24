2ed85dbbfe4261de2dcf20205d2e45cbcc55cbcf
Pigy <cegf.0219@gmail.com>
2023-04-24 17:17:44 -0600

    ProyectoSinNadaMasQueVistaInicialLogin

---
+++ ProyectoV1/Login.swift
@@ -0,0 +1,20 @@
+//
+//  Login.swift
+//  ProyectoV1
+//
+//  Created by ISSC_611_2023 on 24/04/23.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        Login()
+    }
+}
