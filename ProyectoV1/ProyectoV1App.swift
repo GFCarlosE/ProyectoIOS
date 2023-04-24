0efd4bbd37dbc120480583e4625967213c5bcece
Pigy <cegf.0219@gmail.com>
2023-04-24 17:15:19 -0600

    Initial Commit

---
+++ ProyectoV1/ProyectoV1App.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoV1App.swift
+//  ProyectoV1
+//
+//  Created by ISSC_611_2023 on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
