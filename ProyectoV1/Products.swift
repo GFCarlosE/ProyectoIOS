5816da9331be00c197ca77cbeed98131d2c130f9
Axel <axelglez1@hotmail.com>
2023-04-24 18:45:36 -0600

    Products and Purchase added

--- ProyectoV1/Products.swift
+++ ProyectoV1/Products.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct Products: View {
+    @State private var id = ""
+    @State private var name = ""
+    @State private var description = ""
+    @State private var units = ""
+    @State private var cost = ""
+    @State private var price = ""
+    @State private var utility = ""
+    @State private var showAlert = false;
+    @State private var message = "";
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Products").font(.largeTitle).padding()
+
+                TextField("IdProduct",text: $id);
+            
+                TextField("Name",text: $name);
+                TextField("Pieces",text: $description);
+                TextField("IDA",text: $units);
+                TextField("Cost",text: $cost);
+                TextField("Price",text: $price);
+                TextField("Utility",text: $utility);
+            
+            Button("Alta") {
+                
+                if (name != "") {
+                    message = "Creando Cuenta"
+                    //NavigationLink(destination: PerfilView(), label: {Text("ir a la ventana del perfil")}).padding()
+                } else {
+                    message = "Uno o mas campos vacios o contraseña erronea"
+                }
+                showAlert = true
+                
+            }.padding()
+                .alert(isPresented: $showAlert){
+                    Alert(title: Text("Hola"),
+                          message: Text(message))
+                }
+        }
     }
 }
 
--- ProyectoV1/Purchase.swift
+++ ProyectoV1/Purchase.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct Purchase: View {
+    @State private var idProduct = ""
+    @State private var name = ""
+    @State private var pieces = ""
+    @State private var ida = ""
+    @State private var showAlert = false;
+    @State private var message = "";
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        //Color.blue.edgesIgnoringSafeArea(.all)
+        VStack{
+            Text("Purchase").font(.largeTitle).padding()
+            Form{
+                
+                TextField("IdProduct",text: $idProduct);
+                TextField("Name",text: $name);
+                TextField("Pieces",text: $pieces);
+                TextField("IDA",text: $ida);
+                
+            }.padding().scrollContentBackground(.hidden)
+            
+            Button("Buy") {
+                
+                if (name != "") {
+                    message = "Creando Cuenta"
+                    //NavigationLink(destination: PerfilView(), label: {Text("ir a la ventana del perfil")}).padding()
+                } else {
+                    message = "Uno o mas campos vacios o contraseña erronea"
+                }
+                showAlert = true
+                
+            }.padding()
+                .alert(isPresented: $showAlert){
+                    Alert(title: Text("Hola"),
+                          message: Text(message))
+                }
+        }
     }
 }
 
