29ecf7da65d4be79a5b4386c9282b697394e80dc
Pigy <cegf.0219@gmail.com>
2023-04-24 18:42:02 -0600

    SalesAndUserViewsAdded

--- ProyectoV1/Sales.swift
+++ ProyectoV1/Sales.swift
@@ -8,10 +8,61 @@
 import SwiftUI
 
 struct Sales: View {
+    @State private var idproduct = ""
+    @State private var name = ""
+    @State private var quantity = ""
+    @State private var idv = ""
+    @State private var idc = ""
+    @State private var pieces = ""
+    @State private var subtotal = ""
+    @State private var total = ""
+    @State private var mess = ""
+    @State private var mostrarAlerta = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            VStack{
+                Text("Sales").font(.largeTitle).padding()
+                
+                Form{
+                    TextField("IdProduct", text: $idproduct)
+                    TextField("Name", text: $name)
+                    TextField("Quantity", text: $quantity)
+                    TextField("IdV", text: $idv)
+                    TextField("IdC", text: $idc)
+                    TextField("Pieces", text: $pieces)
+                    TextField("Subtotal", text: $subtotal)
+                    TextField("Total", text: $total)
+                }.padding().scrollContentBackground(.hidden)
+                    
+                
+                Button("Generate Sale"){
+                    mostrarAlerta = false
+                    
+                    if (idproduct == "" || name == "" || quantity == "" || idv == "" || idc == "" || pieces == "" || subtotal == "" || total == ""){
+                        idproduct = ""
+                        name = ""
+                        quantity = ""
+                        idv = ""
+                        idc = ""
+                        pieces = ""
+                        subtotal = ""
+                        total = ""
+                        
+                        mess = "At least one field is empty"
+                        mostrarAlerta = true
+                    }else{
+                        mess = "All data accepted"
+                        mostrarAlerta = true
+                    }
+                }.padding()
+                    .alert(isPresented: $mostrarAlerta){
+                        Alert(title: Text("Validation Failed"),message: Text(mess))
+                    }
+                
+            }
+        }
     }
-}
+
+
 
 struct Sales_Previews: PreviewProvider {
     static var previews: some View {
--- ProyectoV1/User.swift
+++ ProyectoV1/User.swift
@@ -8,8 +8,61 @@
 import SwiftUI
 
 struct User: View {
+    @State private var id = ""
+    @State private var name = ""
+    @State private var lastname = ""
+    @State private var age = ""
+    @State private var gender = ""
+    @State private var emailUser = ""
+    @State private var password = ""
+    @State private var mess = ""
+    @State private var mostrarAlerta = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("User").font(.largeTitle).padding()
+            
+            HStack{
+                VStack{
+                    TextField("Id", text: $id)
+                    TextField("Name", text: $name)
+                    TextField("LastName", text: $lastname)
+                    TextField("Age", text: $age)
+                    TextField("Gender", text: $gender)
+                    TextField("Email or User", text: $emailUser)
+                    SecureField("Password", text: $password)
+                }.padding(.horizontal, 100)
+                
+            }
+            
+
+                
+            
+            Button("Generate Sale"){
+                mostrarAlerta = false
+                
+                if (id == "" || name == "" || lastname == "" || age == "" || gender == "" || emailUser == "" || password == ""){
+                    id = ""
+                    name = ""
+                    lastname = ""
+                    age = ""
+                    gender = ""
+                    emailUser = ""
+                    password = ""
+                    
+                    mess = "At least one field is empty"
+                    mostrarAlerta = true
+                }else if(password == "111"){
+                    
+                }else{
+                    mess = "All data accepted"
+                    mostrarAlerta = true
+                }
+            }.padding()
+                .alert(isPresented: $mostrarAlerta){
+                    Alert(title: Text("Validation Failed"),message: Text(mess))
+                }
+            
+        }
     }
 }
 
