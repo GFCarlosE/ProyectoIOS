864c341f289199cdcf17013e292ba3c3be943943
Axel <axelglez1@hotmail.com>
2023-04-26 19:47:35 -0600

    cambios login era

--- ProyectoV1/Login.swift
+++ ProyectoV1/Login.swift
@@ -3,45 +3,54 @@ import SwiftUI
 struct Login: View {
     @State private var user = ""
     @State private var password = ""
+    @State private var mensaje = ""
+    
+    @State private var alerta = false
+    
+   
     
     var body: some View {
-        VStack {
-            Text("Login").font(.largeTitle)
-            
-            Component_TextField(textFieldTitle: "User", textFieldText: $user)
-            
-            Spacer().frame(height: 20)
-            
-            Component_SecureField(secureFieldTitle: "Password", secureFieldText: $password)
-            
-            Spacer().frame(height: 20)
-            
-            Button(action: {
-                // Aquí puedes agregar la acción que se ejecutará al hacer clic en el botón de inicio de sesión
-            }
-            ) {
-                Text("Login")
-                    .foregroundColor(Color.blue)
+        NavigationView{
+            VStack {
+                Text("Login").font(.largeTitle)
+                
+                Component_TextField(textFieldTitle: "User", textFieldText: $user)
+                
+                Spacer().frame(height: 20)
+                
+                Component_SecureField(secureFieldTitle: "Password", secureFieldText: $password)
+                
+                Spacer().frame(height: 20)
+                
+                Button("Login"){
+                    // Aquí puedes agregar la acción que se ejecutará al hacer clic en el botón de inicio de sesión
+                    checkLogin()
+                }.foregroundColor(Color.blue)
                     .font(.headline)
                     .frame(width: 220, height: 60)
-                    .background(.white)
-                    .cornerRadius(15.0)
+                    .alert(isPresented: $alerta){
+                        Alert(title: Text("Alerta"),message:Text("\(mensaje)"),dismissButton: .default(Text("Ok")))
+                    }
+                
+                NavigationLink(destination: User(), label: {
+                    Text("Sign Up").foregroundColor(.white)
+                        .font(.headline)
+                        .frame(width: 220, height: 60)
+                        .background(Color.blue)
+                        .cornerRadius(15.0)
+                })
             }
+            .padding()
             
-            Button(action: {
-                // Aquí puedes agregar la acción que se ejecutará al hacer clic en el botón de inicio de sesión
-            }
-            ) {
-                Text("Register")
-                    .foregroundColor(.white)
-                    .font(.headline)
-                    .frame(width: 220, height: 60)
-                    .background(Color.blue)
-                    .cornerRadius(15.0)
-            }
+          
         }
-        .padding()
+    }
     
+    func checkLogin() {
+        if([user,password].contains("")){
+            mensaje = "Ingresa todos los campos"
+            alerta = true
+       }
     }
 }
 
